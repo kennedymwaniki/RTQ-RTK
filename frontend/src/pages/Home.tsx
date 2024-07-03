ef4124821eb33d19c86a096641b787674c92cdd0
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-07-03 22:46:40 +0300

    broken

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import Container from "../components/Container";
 import Footer from "../components/Footer";
 // import Counter from "../features/counter/Counter"
-// import Login from "../features/login/Login";
+import Login from "../features/login/Login";
 // import Todo from "../features/todo/Todo";
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
     <div>
       <Navbar />
       <Container className="bg-base-200 grid grid-cols-1  place-items-center min-h-screen max-h-full">
-        {/* <Login /> */}
+        <Login />
         {/* <Todo /> */}
         <p>Hello</p>
       </Container>
